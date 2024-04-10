f7d6dde55343cf2404460d375d2897d9731b6bc7
Simon Hartcher <simon@simonhartcher.com>
2024-04-10 13:42:52 +1000

    feat: add buttons enum for input checking

--- src/sdk/system.zig
+++ src/sdk/system.zig
@@ -2,6 +2,15 @@ const std = @import("std");
 const pdapi = @import("playdate_api_definitions.zig");
 const Vector2i = @import("math.zig").Vector2i;
 
+const Buttons = enum(c_int) {
+    ButtonUp = pdapi.BUTTON_UP,
+    ButtonDown = pdapi.BUTTON_DOWN,
+    ButtonLeft = pdapi.BUTTON_LEFT,
+    ButtonRight = pdapi.BUTTON_RIGHT,
+    ButtonA = pdapi.BUTTON_A,
+    ButtonB = pdapi.BUTTON_B,
+};
+
 pub const System = struct {
     api: *const pdapi.PlaydateSys,
 
@@ -13,19 +22,19 @@ pub const System = struct {
         self.api.drawFPS(position.x, position.y);
     }
 
-    pub fn isButtonDown(self: @This(), button: pdapi.PDButtons) bool {
+    pub fn isButtonDown(self: @This(), button: Buttons) bool {
         var down: pdapi.PDButtons = 0;
 
         self.api.getButtonState(&down, null, null);
 
-        return down & button != 0;
+        return down & @intFromEnum(button) != 0;
     }
 
-    pub fn isButtonPressed(self: @This(), button: pdapi.PDButtons) bool {
+    pub fn isButtonPressed(self: @This(), button: Buttons) bool {
         var pressed: pdapi.PDButtons = 0;
 
         self.api.getButtonState(null, &pressed, null);
 
-        return pressed & button != 0;
+        return pressed & @intFromEnum(button) != 0;
     }
 };
